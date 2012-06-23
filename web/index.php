ce3ccf1e5f3e2d7aae4925b828d0be33bba1a289
ksomemo <kntsmy@hotmail.co.jp>
2012-06-23 19:22:26 +0900

    表示用のviewsディレクトリを作成し、そこからファイルを読み込むようにした。

--- web/index.php
+++ web/index.php
@@ -6,14 +6,14 @@ $request_uri = $_SERVER['REQUEST_URI'];
 
 // ルーティングの設定を取得
 $route = array(
-    '/chat/lobby' => 'web/lobby.php'
+    '/chat/lobby' => 'lobby.php'
 );
 
 
 // URIと設定をマッチングさせる
 // ルーティングに対応するファイルを読み込む
 if (isset($route[$request_uri])) {
-    require '../' . $route[$request_uri];
+    require '../views/' . $route[$request_uri];
 } else {
     echo 'not found';
 }
