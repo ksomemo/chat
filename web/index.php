9e46870ee2919b9bf36e29c818dc3c81402cb6ad
ksomemo <kntsmy@hotmail.co.jp>
2012-06-23 18:35:29 +0900

    ルーティングを実装した

--- web/index.php
+++ web/index.php
@@ -1,1 +1,19 @@
-index.php
+<?php
+
+// リクエストURIの取得
+$request_uri = $_SERVER['REQUEST_URI'];
+
+
+// ルーティングの設定を取得
+$route = array(
+    '/chat/lobby' => 'web/lobby.php'
+);
+
+
+// URIと設定をマッチングさせる
+// ルーティングに対応するファイルを読み込む
+if (isset($route[$request_uri])) {
+    require '../' . $route[$request_uri];
+} else {
+    echo 'not found';
+}
