dfa0449ac9cd8de51c82a01c8f53848c2d0cd84f
ksomemo <kntsmy@hotmail.co.jp>
2012-07-26 04:49:22 +0900

    部屋番号を示す変数の名前を変更した。

--- views/lobby.php
+++ views/lobby.php
@@ -1,11 +1,11 @@
 <h1>Lobby</h1>
 
-<?php for ($i = 0; $i < $room_count; $i++) : ?>
-  <div id="room_<?php echo $i ?>">
-    <span class="room_no"><?php echo sprintf("%03d", $i) ?></span>
+<?php for ($room_no = 0; $room_no < $room_count; $room_no++) : ?>
+  <div id="room_<?php echo $room_no ?>">
+    <span class="room_no"><?php echo sprintf("%03d", $room_no) ?></span>
     <span class="room_entrance">
       <a href="entrance.php">入室</a>
     </span>
-    <span class="room_name">部屋名<?php echo $i ?></span>
+    <span class="room_name">部屋名<?php echo $room_no ?></span>
   </div>
 <?php endfor; ?>
