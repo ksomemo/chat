da2423ea255e377c9eb550445be99fba10bba661
ksomemo <kntsmy@hotmail.co.jp>
2012-08-19 08:14:19 +0900

    ステータスに対応するテキストを表示するようにした。

--- core/Response.php
+++ core/Response.php
@@ -5,12 +5,20 @@
 class Response
 {
     /**
+     * ステータスに対応するテキスト
+     * @var array
+     */
+    public static $status_texts = array(
+        404 => 'Not Found',
+    );
+
+    /**
      * httpステータスを設定する
      *
      * @param String $status
      * @param String $text
      */
     public function setHttpStatus($status, $text) {
-        header(sprintf('http/1.1 %s %S', $status, $text));
+        header(sprintf('http/1.1 %s %S', $status, self::$status_texts[$status]));
     }
 }
