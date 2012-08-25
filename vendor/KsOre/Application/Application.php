e3f09104c57853a64d421c9729b7901effe8cb9b
ksomemo <kntsmy@hotmail.co.jp>
2012-08-26 00:06:55 +0900

    アプリケーション実行処理をフロントコントローラから移動した。

--- vendor/KsOre/Application/Application.php
+++ vendor/KsOre/Application/Application.php
@@ -1,5 +1,12 @@
 <?php
 namespace KsOre\Application;
+
+use KsOre\Request\Request;
+use KsOre\Routing\Router;
+use KsOre\Response\Response;
+use KsOre\DBManager\DBManager;
+use KsOre\View\View;
+
 /**
  * Application
  */
@@ -41,5 +48,48 @@ class Application
             'username' => 'chat_user',
             'passwd'   => 'chat_pass'
         );
+    }
+
+    public function run()
+    {
+        // リクエストURIの取得
+        $request = new Request();
+        $request_uri = rtrim($request->getRequestUri(), '/');
+
+
+        // ルーティングの設定を取得
+        $router = new Router($this->getRoutes());
+        $route = $router->getRoute($request_uri);
+
+        $response = new Response();
+
+        // URIと設定をマッチングさせる
+        if ($route) {
+            $controller_name = ucfirst($route['controller']) . 'Controller';
+            require __DIR__.'/../../../src/controllers/'. $controller_name . '.php';
+
+            $db_con_setting = $this->getDbConnectionSetting();
+            $db_manager = new DBManager(
+                    $db_con_setting['key'],
+                    $db_con_setting['dsn'],
+                    $db_con_setting['username'],
+                    $db_con_setting['passwd']
+            );
+
+            $controller = new $controller_name($db_manager);
+            $action_name = $route['action'] . 'Action';
+            $_view_variables = $controller->$action_name($route);
+
+            // 処理結果を表示する
+            $view = new View();
+            $contents = $view->render($route['controller'].'/'.$route['action'], $_view_variables);
+
+        } else {
+            $response->setHttpStatus(404);
+            $contents = 'not found';
+        }
+
+        $response->setContents($contents);
+        $response->send();
     }
 }
