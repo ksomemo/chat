0dd82c4a2898394807193d643b8f2d2405d99626
ksomemo <kntsmy@hotmail.co.jp>
2012-08-26 13:54:01 +0900

    入力値の受け取りを実装した。

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -10,8 +10,20 @@ class UserController extends Controller
     /**
      * 登録
      */
-    function registerAction()
+    public function registerAction()
     {
-        return array();
+        $input = array(
+            'name'      => isset($_POST['name'])      ? $_POST['name'] : '',
+            'mail'      => isset($_POST['mail'])      ? $_POST['mail'] : '',
+            'password'  => isset($_POST['password'])  ? $_POST['password'] : '',
+            'sex'       => isset($_POST['sex'])       ? $_POST['sex'] : '',
+            'year'      => isset($_POST['year'])      ? $_POST['year'] : '',
+            'month'     => isset($_POST['month'])     ? $_POST['month'] : '',
+            'day'       => isset($_POST['day'])       ? $_POST['day'] : '',
+            'agreement' => isset($_POST['agreement']) ? $_POST['agreement'] : '',
+        );
+
+        return array('input' => $input);
     }
+
 }
--- src/views/user/register.php
+++ src/views/user/register.php
@@ -2,48 +2,48 @@
 <form action="" method="post">
     <div>
         <label for="name">名前</label>
-        <input type="text" name="name" id="name" value="">
+        <input type="text" name="name" id="name" value="<?php echo $input['name'] ?>">
     </div>
 
     <div>
         <label for="mail">メールアドレス</label>
-        <input type="text" name="mail" id="mail" value="">
+        <input type="text" name="mail" id="mail" value="<?php echo $input['mail'] ?>">
     </div>
 
     <div>
         <label for="password">パスワード</label>
-        <input type="password" name="password" id="password" value="">
+        <input type="password" name="password" id="password" value="<?php echo $input['password'] ?>">
     </div>
 
     <div>
         <label>性別</label>
-        <input type="radio" name="sex" id="sex_1" value="1">
+        <input type="radio" name="sex" id="sex_1" value="1" <?php if ($input['sex'] == 1) echo 'checked' ?>>
         <label for="sex_1">男</label>
-        <input type="radio" name="sex" id="sex_2" value="2">
+        <input type="radio" name="sex" id="sex_2" value="2" <?php if ($input['sex'] == 2) echo 'checked' ?>>
         <label for="sex_2">女</label>
     </div>
 
     <div>
         <label>生年月日</label>
         <select name="year">
-            <option value="1980">1980</option>
-            <option value="1990">1990</option>
-            <option value="2000">1990</option>
+            <option value="1980" <?php if ($input['year'] == 1980) echo 'selected' ?>>1980</option>
+            <option value="1990" <?php if ($input['year'] == 1990) echo 'selected' ?>>1990</option>
+            <option value="2000" <?php if ($input['year'] == 2000) echo 'selected' ?>>2000</option>
         </select>
         <select name="month">
-            <option value="1">1</option>
-            <option value="6">6</option>
-            <option value="12">12</option>
+            <option value="1"  <?php if ($input['month'] == 1)  echo 'selected' ?>>1</option>
+            <option value="6"  <?php if ($input['month'] == 6)  echo 'selected' ?>>6</option>
+            <option value="12" <?php if ($input['month'] == 12) echo 'selected' ?>>12</option>
         </select>
         <select name="day">
-            <option value="1">1</option>
-            <option value="15">15</option>
-            <option value="30">30</option>
+            <option value="1"  <?php if ($input['day'] == 1)  echo 'selected' ?>>1</option>
+            <option value="15" <?php if ($input['day'] == 15) echo 'selected' ?>>15</option>
+            <option value="30" <?php if ($input['day'] == 30) echo 'selected' ?>>30</option>
         </select>
     </div>
 
     <div>
-        <input type="checkbox" name="agreement" id="agreement" value="1">
+        <input type="checkbox" name="agreement" id="agreement" value="1"  <?php if ($input['agreement'] == 1) echo 'checked' ?>>
         <label for="agreement">規約に同意</label>
     </div>
 
