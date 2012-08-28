8a77a1d511ea09a0ba9cea08a8f4f812d4e26b33
ksomemo <kntsmy@hotmail.co.jp>
2012-08-28 23:41:24 +0900

    スカラ値および、配列に含まれるスカラ値に対するエスケープ処理を実装した。

--- vendor/KsOre/View/View.php
+++ vendor/KsOre/View/View.php
@@ -14,8 +14,12 @@ class View
      *
      * @return String $_all_contens
      */
-    function render($_path, $_variables)
+    public function render($_path, $_variables)
     {
+        foreach ($_variables as $key => $value) {
+            $_variables[$key] = $this->recursiveEscape($value);
+        }
+
         extract($_variables);
 
         ob_start();
@@ -27,5 +31,35 @@ class View
         $_all_contents = ob_get_clean();
 
         return $_all_contents;
+    }
+
+    /**
+     * XSS対策のためにエスケープする
+     *
+     * @param mixed $value
+     *
+     * @return string
+     */
+    public function escape($value)
+    {
+        if (is_bool($value) || is_numeric($value) || $value === null || is_object($value) || is_array($value)) {
+            // do nothing
+            return $value;
+        }
+
+        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+    }
+
+    /**
+     * 配列のための再帰的エスケープ
+     * @param mixed $value
+     *
+     * @return mixed(array|string)
+     */
+    public function recursiveEscape($value)
+    {
+        return is_array($value) ?
+               array_map(array($this, 'recursiveEscape'), $value) :
+               $this->escape($value);
     }
 }
